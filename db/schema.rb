db74ea31f33b56cde7048f95f2100bde09888f3a
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-07-23 13:10:42 +0200

    Added category to treatments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_093318) do
+ActiveRecord::Schema.define(version: 2019_07_23_110919) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(version: 2019_07_23_093318) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
     t.index ["provider_id"], name: "index_treatments_on_provider_id"
   end
 
@@ -58,6 +59,8 @@ ActiveRecord::Schema.define(version: 2019_07_23_093318) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "avatar"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

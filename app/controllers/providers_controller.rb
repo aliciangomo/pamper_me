4eaee506b145840f25fb93d42eaae9511dff36a6
Adam McCallister <adam@adammccallister.com>
2019-07-24 20:08:31 +0200

    Initial commit on branch

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -33,11 +33,23 @@ class ProvidersController < ApplicationController
   def show
     set_provider
     @booking = Booking.new
+    tel_to(@provider.phone_number)
     # When multiple photos are added
     # @photos = @painting.photos.all
   end
 
   private
+
+  def tel_to(text)
+    groups = text.to_s.scan(/(?:^\+)?\d+/)
+    if groups.size > 1 && groups[0][0] == '+'
+      # remove leading 0 in area code if this is an international number
+      groups[1] = groups[1][1..-1] if groups[1][0] == '0'
+      groups.delete_at(1) if groups[1].size == 0 # remove if it was only a 0
+    end
+    # groups = text.to_s.scan(/(?:^\+)?\d+/)
+    @phone_number = "tel:#{groups.join '-'}"
+  end
 
   def set_provider
     @provider = Provider.find(params[:id])

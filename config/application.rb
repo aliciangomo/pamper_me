e04a06dd944041cab651b03c765563b6eac60003
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-07-27 20:12:27 +0200

    esm fix

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,6 @@ module PamperMe
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.assets.initialize_on_precompile = false
   end
 end

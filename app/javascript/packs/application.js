82b6fdc708ca46543966d9a6b2e69fffd96c0e48
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-07-29 17:15:03 +0200

    added stats to provider's dashboard, amended map toggle in index, added all options for search message in index, corrected the icon in booking show page and the icons in providers page

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+
+
 import { Application } from "stimulus";
 import { definitionsFromContext } from "stimulus/webpack-helpers";
 
@@ -7,18 +9,18 @@ import { initMapbox } from '../plugins/init_mapbox';
 
 import { colorStatus } from '../components/user_dashboard.js';
 
-import {toggleCollapsible} from '../components/collapsible.js'
+import {toggleCollapsible} from '../components/collapsible.js';
 
-import {toggleDropdown} from '../components/user_dashboard_dropdown.js'
+import {toggleDropdown} from '../components/user_dashboard_dropdown.js';
 
-import {renderMap} from '../components/search_map.js'
+import {renderMap} from '../components/search_map.js';
 
-import {mapsSelector} from '../components/maplink.js'
-window.mapsSelector = mapsSelector
+import {mapsSelector} from '../components/maplink.js';
+window.mapsSelector = mapsSelector;
 
-const application = Application.start()
-const context = require.context("./controllers", true, /\.js$/)
-application.load(definitionsFromContext(context))
+const application = Application.start();
+const context = require.context("./controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
 
 renderMap();
 initMapbox();

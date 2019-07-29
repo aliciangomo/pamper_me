fec27aee2438ed637e4f54325d62be82e512efe3
Alicia Ngomo <alicia_ngomo@hotmail.com>
2019-07-29 11:38:20 +0200

    fix app settings

--- config/application.rb
+++ config/application.rb
@@ -15,14 +15,11 @@ module PamperMe
         end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.assets.initialize_on_precompile = true
-    # config.paths.add File.join('app', 'services'), glob: File.join('**', '*.rb')
-    # config.autoload_paths += Dir[Rails.root.join('app', 'services', '*')]
+    config.action_view.embed_authenticity_token_in_remote_forms = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
